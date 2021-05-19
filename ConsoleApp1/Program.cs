1fd7ba0bb08bcf3deb3cf213349e761551e11c45
Dariusz Krasowski <dariuszkrasowski95@outlook.com>
2021-05-19 21:15:58 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--window-size=1200,800");
+            IWebDriver driver = new ChromeDriver(Environment.CurrentDirectory,options);
+            driver.Navigate().GoToUrl("http://www.wp.pl/");
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
